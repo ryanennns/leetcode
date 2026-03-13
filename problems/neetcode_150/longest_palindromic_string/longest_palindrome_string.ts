e0b9d300c4a5d5a0792d96a160101c2b89c410f0
Ryan Enns <ryanennns@gmail.com>
2026-03-13 16:26:05 -0400

    longest palindrome solution

--- problems/neetcode_150/longest_palindromic_string/longest_palindrome_string.ts
+++ problems/neetcode_150/longest_palindromic_string/longest_palindrome_string.ts
@@ -1,19 +1,24 @@
 export const longestPalindrome = (s: string): string => {
   let longestPalindrome = "";
+  const setLongestPalindrome = (candidate: string) =>
+    (longestPalindrome =
+      candidate.length > longestPalindrome.length
+        ? candidate
+        : longestPalindrome);
 
-  for (let i = 0; i <= s.length; i++) {
-    for (let j = i; j <= s.length; j++) {
-      let substring = s.slice(i, j);
-      if (substring === substring.split("").reverse().join('')) {
-        longestPalindrome =
-          longestPalindrome.length > substring.length
-            ? longestPalindrome
-            : substring;
-      }
+  const findLongestPalindrome = (s: string, l: number, r: number) => {
+    while (s[l] === s[r]) {
+      setLongestPalindrome(s.slice(l, r + 1));
+      l-- && r++;
     }
+  };
+
+  for (let i = 0; i < s.length; i++) {
+    findLongestPalindrome(s, i, i);
+    findLongestPalindrome(s, i, i + 1);
   }
 
   return longestPalindrome;
 };
 
-console.log(longestPalindrome("ibvjkmpyzsifuxcabqqpahjdeuzaybqsrsmbfplxycsafogotliyvhxjtkrbzqxlyfwujzhkdafhebvsdhkkdbhlhmaoxmbkqiwiusngkbdhlvxdyvnjrzvxmukvdfobzlmvnbnilnsyrgoygfdzjlymhprcpxsnxpcafctikxxybcusgjwmfklkffehbvlhvxfiddznwumxosomfbgxoruoqrhezgsgidgcfzbtdftjxeahriirqgxbhicoxavquhbkaomrroghdnfkknyigsluqebaqrtcwgmlnvmxoagisdmsokeznjsnwpxygjjptvyjjkbmkxvlivinmpnpxgmmorkasebngirckqcawgevljplkkgextudqaodwqmfljljhrujoerycoojwwgtklypicgkyaboqjfivbeqdlonxeidgxsyzugkntoevwfuxovazcyayvwbcqswzhytlmtmrtwpikgacnpkbwgfmpavzyjoxughwhvlsxsgttbcyrlkaarngeoaldsdtjncivhcfsaohmdhgbwkuemcembmlwbwquxfaiukoqvzmgoeppieztdacvwngbkcxknbytvztodbfnjhbtwpjlzuajnlzfmmujhcggpdcwdquutdiubgcvnxvgspmfumeqrofewynizvynavjzkbpkuxxvkjujectdyfwygnfsukvzflcuxxzvxzravzznpxttduajhbsyiywpqunnarabcroljwcbdydagachbobkcvudkoddldaucwruobfylfhyvjuynjrosxczgjwudpxaqwnboxgxybnngxxhibesiaxkicinikzzmonftqkcudlzfzutplbycejmkpxcygsafzkgudy"));
+console.log(longestPalindrome("cbbd"));
