f18a7d4c25c35bf9b53b66c3e3c0a2f12076f6c4
Ryan Enns <ryanennns@gmail.com>
2026-03-25 13:45:13 -0400

    coin change

---
+++ problems/neetcode_150/coin_change_ii/coin_change.ts
@@ -0,0 +1,22 @@
+export const change = (amount: number, coins: number[]): number => {
+  let possibleCombinations = 0;
+
+  const recurse = (current: number, available: number[]) => {
+    for (let i = 0; i < available.length; i++) {
+      const newCurrent = current - available[i];
+
+      const possible = available.filter(c => c <= newCurrent);
+      if (possible.length > 0) {
+        recurse(newCurrent, possible);
+      }
+
+      if (newCurrent === 0) {
+        possibleCombinations += 1;
+      }
+    }
+  }
+
+  recurse(amount, coins);
+
+  return possibleCombinations;
+};
