245ae5b76f06e8485c16fa2de4879f25afb10178
Ryan Enns <ryanennns@gmail.com>
2026-04-07 17:50:54 -0400

    can jump

---
+++ problems/neetcode_150/can_jump/can_jump.ts
@@ -0,0 +1,13 @@
+export const canJump = (nums: number[]): boolean => {
+  let goalpost = nums.length - 1
+
+  for (let i = goalpost; i > 0; i--) {
+    const previousNumber = nums[i - 1];
+
+    if (previousNumber > (goalpost - i)) {
+      goalpost = i - 1;
+    }
+  }
+
+  return goalpost === 0;
+};
