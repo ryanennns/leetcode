e6b9065200920e0662270abc90c0b1285aa47b15
Ryan Enns <ryanennns@gmail.com>
2026-04-07 17:50:51 -0400

    3sum diff??

--- problems/neetcode_150/3sum/3sum.ts
+++ problems/neetcode_150/3sum/3sum.ts
@@ -1,20 +1,28 @@
 const threeSum = (nums: Array<number>) => {
-  const combinations = new Set<string>();
+  let combinations = new Set<string>();
 
   for (let i = 0; i < nums.length; i++) {
-    for (let j = i; j < nums.length; j++) {
-      for (let k = j; k < nums.length; k++) {
-        if (
-            nums[i] + nums[j] + nums[k] === 0 &&
-            i !== j && i !== k && j !== k
-        ) {
-          combinations.add([nums[i], nums[j], nums[k]].sort().join())
-        }
+    let currentSum = nums[i];
+
+    console.log(currentSum);
+    for (let j = 0; j <= Math.abs(currentSum); j++) {
+      if (currentSum < 0) {
+        let l = j;
+        let r = currentSum + j;
+
+        console.log([l, r]);
+      }
+
+      if (currentSum > 0) {
+        let l = j;
+        let r = currentSum + j;
+
+        console.log([l, r]);
       }
     }
   }
 
-  return [...combinations].map(v=>v.split(",").map(v=>Number(v)))
+  return combinations.size;
 };
 
-console.log(threeSum([-1,0,1,2,-1,-4]))
+console.log(threeSum([-1, 0, 1, 2, -1, -4]));
