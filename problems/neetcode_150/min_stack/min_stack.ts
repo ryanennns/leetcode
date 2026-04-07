362e6b0e949c67539d795cbc38423b2d1f350f6e
Ryan Enns <ryanennns@gmail.com>
2026-04-07 18:01:40 -0400

    min stack

---
+++ problems/neetcode_150/min_stack/min_stack.ts
@@ -0,0 +1,32 @@
+class MinStack {
+  nums: Array<number> = [];
+
+  constructor() {
+
+  }
+
+  push(val: number): void {
+    this.nums.push(val);
+  }
+
+  pop(): void {
+    const value = this.nums.pop();
+  }
+
+  top(): number {
+    return this.nums[this.nums.length - 1];
+  }
+
+  getMin(): number {
+    return Math.min(...this.nums);
+  }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
